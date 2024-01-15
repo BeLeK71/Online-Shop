e11e3339e955aacd54b14d4f0cc526f2626f2e60
Belek Urmatov <belekurmatov00@gmail.com>
2024-01-15 20:48:32 +0600

    first commit

--- src/components/homePage/Footer.jsx
+++ src/components/homePage/Footer.jsx
@@ -2,41 +2,48 @@ import React from "react";
 import img1 from "./assets/twitter-svgrepo-com.svg";
 import img2 from "./assets/371164.svg";
 import img3 from "./assets/iconmonstr-github-1.svg";
+import img4 from "./assets/Line 14.svg";
+import "./Footer.css";
 
 const MyStyledDiv = () => {
-  const divStyle = {
-    display: "flex",
-    justifyContent: "space-between", // Adjust this property based on your design needs
-    alignItems: "center", // Align items vertically in the center
-    width: "100%",
-    height: "159px",
-    flexShrink: 0,
-    alignSelf: "flex-end",
-  };
-
-  const h1Style = {
-    color: "var(--Light-Colors-Dark-gray---Light, #707070)",
-    fontFamily: "DM Sans",
-    fontSize: "16px",
-    fontStyle: "normal",
-    fontWeight: "400",
-    lineHeight: "27px",
-  };
-  const imgStyle = {
-    width: "156px",
-    height: "19px",
-    flexShrink: 0,
-  };
-
   return (
-    <div style={divStyle}>
-      <h1 style={h1Style}>CONTACT</h1>
-      <h1 style={h1Style}>TERMS OF SERVICES</h1>
-      <h1 style={h1Style}>SHIPPING AND RETURNS</h1>
-      <img src={img1} style={imgStyle} />
-      <img src={img2} style={imgStyle} />
-      <img src={img3} style={imgStyle} />
-      <h3>© 2021 Shelly. Terms of use and privacy policy.</h3>
+    <div id="main_div">
+      <img src={img4} alt="Line" id="img_line" />
+      <div id="div_2">
+        <h1>
+          <a href="#contact" id="h1">
+            CONTACT
+          </a>
+        </h1>
+        <h1>
+          <a href="#terms" id="h1">
+            TERMS OF SERVICES
+          </a>
+        </h1>
+        <h1>
+          <a href="#shipping" id="h1">
+            SHIPPING AND RETURNS
+          </a>
+        </h1>
+      </div>
+      <div id="social_media">
+        <a
+          href="https://twitter.com/elonmusk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+          target="_blank"
+        >
+          <img src={img1} alt="Описание изображения 1" id="img_id" />
+        </a>
+
+        <a href="https://www.instagram.com/adeliya.gold/" target="_blank">
+          <img src={img2} alt="Описание изображения 2" id="img_id" />
+        </a>
+
+        <a href="https://github.com/BeLeK71/Online-Shop" target="_blank">
+          <img src={img3} alt="Описание изображения 3" id="img_id" />
+        </a>
+      </div>
+
+      <h3 id="h3">© 2021 Shelly. Terms of use and privacy policy.</h3>
     </div>
   );
 };
