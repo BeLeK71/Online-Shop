6504763d8ecf9641c039bf88c6151c2470e062d2
wiygdiwgw <alikhanchik1582@gmail.com>
2024-01-17 20:03:53 +0600

    myCommit

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useState } from "react";
+import PaginationControlled from "./Pagination";
+import { useProducts } from "../context/ProductContextProvider";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const { getProducts, products } = useProducts();
+  //! PAGINATION
+  const [page, setPage] = useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+  const itemPerPage = 3;
+  const count = Math.ceil(products.length / itemPerPage);
+  console.log(count);
+  function currentData() {
+    const begin = (page - 1) * itemPerPage;
+    const end = begin + itemPerPage;
+    return products.slice(begin, end);
+  }
+  console.log(currentData());
+  return (
+    <Box>
+      <PaginationControlled
+        handleChange={handleChange}
+        page={page}
+        count={count}
+      />
+    </Box>
+  );
 };
 
 export default ProductList;
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ProductsList from "../components/product/ProductList";
 
 const ProductPage = () => {
-  return <div>ProductPage</div>;
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div style={{ width: "300px", flex: "none" }}></div>
+      <ProductsList />
+    </div>
+  );
 };
 
 export default ProductPage;
