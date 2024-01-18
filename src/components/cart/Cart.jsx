0c40c270afcde6ccdc13c0919c9ffd698a3c465b
Belek Urmatov <belekurmatov00@gmail.com>
2024-01-18 18:57:54 +0600

    first commit

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,52 +1,31 @@
-import {
-  Box,
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Typography, Input, Grid } from "@mui/material";
 import React, { useEffect } from "react";
 import img1 from "./One_Ring_Blender_Render.png";
+import img2 from "./Vector.svg";
+import img3 from "./Line 01.svg";
 
 const Cart = () => {
   const cartCleaner = () => {
     localStorage.removeItem("cart");
     // getCart();
   };
+
   return (
-    // <TableContainer>
-    //   <Table sx={{ minWidth: 650 }} aria-label="simple table">
-    //     <TableHead>
-    //       <TableRow>
-    //         <TableCell>Picture</TableCell>
-    //       </TableRow>
-    //     </TableHead>
-    //     <TableBody></TableBody>
-    //   </Table>
-    //   <Button onClick={cartCleaner}>BYE NOW FOR </Button>
-    // </TableContainer>
     <Box
       sx={{
-        // width: "50vw",
-        // height: 500,
         margin: "20px auto",
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
+        padding: "0 10px",
       }}
     >
       <Typography
-        id
         variant="h4"
         align="center"
         sx={{
           fontFamily: "DM Sans",
           fontSize: "33px",
-          fontStyle: "normal",
           fontWeight: 500,
           lineHeight: "43px",
         }}
@@ -54,15 +33,10 @@ const Cart = () => {
         Shopping Cart
       </Typography>
 
-      <div>
-        <div
-          id="left" // дальше надо будет заменить все дефолд значения на значения из баззы
-          данных
-        >
-          <div
-            id="card"
-            style={{ width: "580px", marginLeft: "5%", display: "flex" }}
-          >
+      <Grid container spacing={2} sx={{ marginTop: "20px" }}>
+        {/* left window start */}
+        <Grid item xs={12} md={6}>
+          <div id="card" style={{ display: "flex" }}>
             <img
               src={img1}
               alt=""
@@ -80,24 +54,91 @@ const Cart = () => {
               <div style={{ display: "flex" }}>Category</div>
               <div style={{ display: "flex" }}>Price</div>
             </div>
-            {/* Добавления кол-ва <TableCell align="right">
-              <input type="number" min={1} max={20} />
-            </TableCell> */}
-            {/* <TableCell align="right">shit</TableCell> */}
-            <input
+
+            <Input
               style={{
-                width: "10%",
-                height: "20%",
+                width: "5%",
+                height: "8%",
                 flexShrink: 0,
-                marginLeft: "8%",
+                marginLeft: "10%",
                 marginTop: "2%",
               }}
               type="number"
             />
+            <img
+              src={img2}
+              alt="x"
+              style={{
+                display: "flex",
+                width: "2%",
+                marginLeft: "5%",
+                marginTop: "-13%",
+              }}
+            />
           </div>
-        </div>
-        <div id="right"></div>
-      </div>
+
+          <Button
+            id="coupon"
+            style={{
+              width: "50%",
+              height: "53px",
+              flexShrink: 0,
+              borderRadius: "4px",
+              border: "1px solid var(--Light-Colors-Black---Light, #000)",
+              background: "var(--Light-Colors-Black---Light, #000)",
+              color: "white",
+              marginTop: "5%",
+            }}
+          >
+            COUPON
+          </Button>
+          {/* левое окно finish */}
+        </Grid>
+
+        {/* right window start */}
+        <Grid item xs={12} md={6}>
+          <div id="card-2">
+            <Typography
+              variant="h5"
+              sx={{
+                color: "var(--Light-Colors-Black---Light, #000)",
+                fontFamily: "DM Sans",
+                fontSize: "26px",
+                fontWeight: 400,
+                lineHeight: "25px",
+                marginBottom: "15px",
+              }}
+            >
+              Cart Totals
+            </Typography>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginBottom: "15px",
+              }}
+            >
+              <Typography variant="body1">TOTAL</Typography>
+              <Typography variant="body1">//Price</Typography>
+            </div>
+            <Button
+              id="checkout"
+              style={{
+                width: "50%",
+                height: "53px",
+                flexShrink: 0,
+                borderRadius: "4px",
+                border: "1px solid var(--Light-Colors-Black---Light, #000)",
+                background: "var(--Light-Colors-Black---Light, #000)",
+                color: "white",
+                marginTop: "5%",
+              }}
+            >
+              CHECKOUT
+            </Button>
+          </div>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
--- src/components/context/ProductContextProvider.js
+++ src/components/context/ProductContextProvider.js
@@ -1,8 +1,48 @@
-import React, { useContext } from "react";
-export const useProducts = () => useContext;
+import axios from "axios";
+import React, { createContext, useContext, useReducer } from "react";
+import { useNavigate } from "react-router-dom";
+export const productContext = createContext();
+export const useProducts = () => useContext(productContext);
 
-const ProductContextProvider = () => {
-  return <div>ProductContextProvider</div>;
+const INIT_STATE = {
+  products: [],
+  oneProduct: null,
+  categories: [],
+};
+
+const reducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case ACTIONS.GET_PRODUCTS:
+      return { ...state, products: action.payload };
+    case ACTIONS.GET_ONE_PRODUCT:
+      return { ...state, oneProduct: action.payload };
+    case ACTIONS.GET_CATEGORIES:
+      return { ...state, categories: action.payload };
+  }
+};
+
+const ProductContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [state, dispatch] = useReducer(reducer, INIT_STATE);
+
+  //CREATE
+  const addProduct = async (newProduct) => {
+    await axios.post(API, newProduct);
+    navigate("/products");
+  };
+
+  //READ
+  const getProducts = async () => {
+    const { data } = await axios(`${API}${window.location.search}`);
+    dispatch({
+      type: ACTIONS.GET_PRODUCTS,
+      payload: data,
+    });
+  };
+  //DELETE
+  const deleteProduct = async (id) => {
+    await axios.delete(`${API}/${id}`);
+  };
 };
 
 export default ProductContextProvider;
